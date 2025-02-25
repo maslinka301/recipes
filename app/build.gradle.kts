63713715080d7d16b21ff6d237bc3c2b928f7030
maslinka301 <maslinka_301@mail.ru>
2025-02-26 00:17:00 +0300

    icon image added, minSdk changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.maslinka.recipes"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
