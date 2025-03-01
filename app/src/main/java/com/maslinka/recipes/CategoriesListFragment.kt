1377430430a58b990696748acea5ba4807bd8f7f
maslinka301 <maslinka_301@mail.ru>
2025-03-01 19:57:42 +0300

    RA-04 Creating the first fragment

---
+++ app/src/main/java/com/maslinka/recipes/CategoriesListFragment.kt
@@ -0,0 +1,27 @@
+package com.maslinka.recipes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.maslinka.recipes.databinding.FragmentListCategoriesBinding
+
+class CategoriesListFragment:Fragment() {
+
+    var _binding : FragmentListCategoriesBinding? = null
+    val binding
+        get() = _binding ?: throw IllegalStateException("binding не инициализировано")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListCategoriesBinding.inflate(inflater)
+        return binding.root
+    }
+
+
+
+}
--- app/src/main/java/com/maslinka/recipes/MainActivity.kt
+++ app/src/main/java/com/maslinka/recipes/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.maslinka.recipes.databinding.ActivityMainBinding
+import com.maslinka.recipes.CategoriesListFragment as CategoriesListFragment
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -17,5 +22,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportFragmentManager.commit {
+            add<CategoriesListFragment>(binding.fragmentContainerView.id)
+        }
     }
 }
