178a0b0fde336ced0bf6a6c86b04b5948a495b45
maslinka301 <maslinka_301@mail.ru>
2025-03-06 19:01:42 +0300

    RA-08 Creating recyclerView with a list of categories

--- app/src/main/java/com/maslinka/recipes/CategoriesListFragment.kt
+++ app/src/main/java/com/maslinka/recipes/CategoriesListFragment.kt
@@ -22,9 +22,18 @@ class CategoriesListFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecycler()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun initRecycler() {
+        binding.rvCategories.adapter = CategoriesListAdapter(STUB.getCategories())
     }
 
 }
