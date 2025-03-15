2acb4eebc8f0c7780081e6c0cf8e151a0de4fd53
maslinka301 <maslinka_301@mail.ru>
2025-03-15 16:13:25 +0300

    RA-11 Created common recycler, a screen with a list of recipes and a screen of recipe

--- app/src/main/java/com/maslinka/recipes/Constants.kt
+++ app/src/main/java/com/maslinka/recipes/Constants.kt
@@ -4,5 +4,6 @@ object Constants {
     const val ARG_CATEGORY_ID = "ARG_CATEGORY_ID"
     const val ARG_CATEGORY_NAME = "ARG_CATEGORY_NAME"
     const val ARG_CATEGORY_IMAGE_URL = "ARG_CATEGORY_IMAGE_URL"
+    const val ARG_RECIPE_ID = "ARG_RECIPE_ID"
 
 }
--- app/src/main/java/com/maslinka/recipes/RecipesListFragment.kt
+++ app/src/main/java/com/maslinka/recipes/RecipesListFragment.kt
@@ -6,21 +6,25 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.maslinka.recipes.Constants.ARG_CATEGORY_ID
 import com.maslinka.recipes.Constants.ARG_CATEGORY_IMAGE_URL
 import com.maslinka.recipes.Constants.ARG_CATEGORY_NAME
+import com.maslinka.recipes.Constants.ARG_RECIPE_ID
 import com.maslinka.recipes.databinding.FragmentListRecipesBinding
 import java.io.IOException
 
-class RecipesListFragment: Fragment() {
+class RecipesListFragment : Fragment() {
     private var _binding: FragmentListRecipesBinding? = null
     private val binding
         get() = _binding ?: throw IllegalStateException("binding не инициализировано")
 
-    private var categoryId:Int? = null
-    private var categoryName:String? = null
-    private var categoryImageUrl:String? = null
+    private var categoryId: Int? = null
+    private var categoryName: String? = null
+    private var categoryImageUrl: String? = null
 
 
     override fun onCreateView(
@@ -45,32 +49,45 @@ class RecipesListFragment: Fragment() {
         _binding = null
     }
 
-    private fun initBundleData(){
+    private fun initBundleData() {
         arguments?.let { arguments ->
             categoryId = arguments.getInt(ARG_CATEGORY_ID)
             categoryName = arguments.getString(ARG_CATEGORY_NAME)
             categoryImageUrl = arguments.getString(ARG_CATEGORY_IMAGE_URL)
-        }?: throw IllegalStateException("Arguments are null")
+        } ?: throw IllegalStateException("Arguments are null")
     }
 
-    private fun initRecycler(categoryId: Int){
-        val adapter = RecipeListAdapter(STUB.getRecipesByCategoryId(categoryId))
+    private fun initRecycler(categoryId: Int) {
+        val adapter = RecyclerViewsAdapter(STUB.getRecipesByCategoryId(categoryId))
         binding.rvRecipes.adapter = adapter
+        adapter.setOnItemClickListener(object : RecyclerViewsAdapter.OnItemClickListener {
+            override fun onItemClick(itemId: Int) {
+                openRecipeByRecipeId(itemId)
+            }
+        })
     }
 
-    private fun getCategoryImageFromAssets() : Drawable?{
+    private fun getCategoryImageFromAssets(): Drawable? {
         val drawable =
             try {
                 Drawable.createFromStream(
                     categoryImageUrl?.let { binding.root.context.assets.open(it) },
                     null
                 )
-            }
-            catch (e: IOException){
+            } catch (e: IOException) {
                 Log.e("!!!", "Error loading image from assets", e)
                 e.printStackTrace()
                 null
             }
         return drawable
+    }
+
+    fun openRecipeByRecipeId(recipeId: Int) {
+        parentFragmentManager.commit {
+            replace<RecipeFragment>(R.id.fragmentContainerView)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
+
     }
 }
