f9a9b54a0b265ba1a3ac1db624d7dbde86edaf1e
maslinka301 <maslinka_301@mail.ru>
2025-05-25 16:39:01 +0300

    RA-32 Реализован запрос через URLConnection

--- app/src/main/java/com/maslinka/recipes/model/Category.kt
+++ app/src/main/java/com/maslinka/recipes/model/Category.kt
@@ -2,8 +2,10 @@ package com.maslinka.recipes.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
 @Parcelize
+@Serializable
 data class Category(
     val id: Int,
     val title: String,
